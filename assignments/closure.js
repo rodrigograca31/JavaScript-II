5322cab672c78790995447d832fa772abe74ece8
rodrigograca31 <rodrigograca1993@gmail.com>
2019-10-15 12:31:03 +0100

    Formatting

--- assignments/closure.js
+++ assignments/closure.js
@@ -10,12 +10,12 @@
 
 // ==== Challenge 2: Implement a "counter maker" function ====
 const counterMaker = () => {
-  // IMPLEMENTATION OF counterMaker:
-  // 1- Declare a `count` variable with a value of 0. We will be mutating it, so declare it using `let`!
-  // 2- Declare a function `counter`. It should increment and return `count`.
-  //      NOTE: This `counter` function, being nested inside `counterMaker`,
-  //      "closes over" the `count` variable. It can "see" it in the parent scope!
-  // 3- Return the `counter` function.
+	// IMPLEMENTATION OF counterMaker:
+	// 1- Declare a `count` variable with a value of 0. We will be mutating it, so declare it using `let`!
+	// 2- Declare a function `counter`. It should increment and return `count`.
+	//      NOTE: This `counter` function, being nested inside `counterMaker`,
+	//      "closes over" the `count` variable. It can "see" it in the parent scope!
+	// 3- Return the `counter` function.
 };
 // Example usage: const myCounter = counterMaker();
 // myCounter(); // 1
@@ -27,7 +27,7 @@ const counterMaker = () => {
 
 // ==== Challenge 4: Create a counter function with an object that can increment and decrement ====
 const counterFactory = () => {
-  // Return an object that has two methods called `increment` and `decrement`.
-  // `increment` should increment a counter variable in closure scope and return it.
-  // `decrement` should decrement the counter variable and return it.
+	// Return an object that has two methods called `increment` and `decrement`.
+	// `increment` should increment a counter variable in closure scope and return it.
+	// `decrement` should decrement the counter variable and return it.
 };
